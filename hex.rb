fb5fe4ba13e45751c9aad3bfcdf76bf11c8f98be
Nathan Baker <nathan.baker@sysdig.com>
2019-01-25 10:02:47 -0500

    Made hex actually do what it's supposed to

--- hex.rb
+++ hex.rb
@@ -1,6 +1,10 @@
 #!/usr/bin/ruby
 
 while (i = ARGV.shift)
-	puts "#{i} => 0x#{i.to_i.to_s(16)}"
+	if i[0..1] == '0x'
+		puts "#{i} => #{i[2..-1].to_i(16)}"
+	else
+		puts "#{i} => 0x#{i.to_i.to_s(16)}"
+	end
 end
 
