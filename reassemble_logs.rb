07b67518649882b57ec20ac3e1105b51eafb398b
Nathan Baker <nathan.baker@sysdig.com>
2022-03-05 20:42:39 -0500

    Handle bogus timestamps

--- reassemble_logs.rb
+++ reassemble_logs.rb
@@ -6,8 +6,13 @@ h = {}
 
 ARGV.each { |f|
   ft = File.read(f)
-  ts = DateTime.parse(ft[0..24])
-  h.store(ts, ft)
+  ts = nil
+  begin
+    ts = DateTime.parse(ft[0..24])
+    h.store(ts, ft)
+  rescue
+    STDERR.puts "Could not parse #{ft[0..24]} for file #{f}, skipping"
+  end
 }
 
 h.keys.sort.each { |k|
