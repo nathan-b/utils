235a8adf20e22e9b04140385d8dd434737b3793d
Nathan Baker <nathan.baker@sysdig.com>
2022-10-18 11:51:59 -0400

    Updated histogram tool

--- analyzer_histogram.rb
+++ analyzer_histogram.rb
@@ -23,39 +23,57 @@ def get_bucket(f)
   end
 end
 
-# Open and read the file
-ftext = File.read(ARGV.shift)
-buckets = []
-values = []
-curr_bucket = 0
-curr_bucket_desc = nil
-ftext.each_line { |line|
-  if line =~ /c=([0-9.]+),/
-    str_val = $1
-    f = Float(str_val)
-    bucket = get_bucket(f)
-    next if bucket == 0
-    values << f
-    if bucket == curr_bucket
-      curr_bucket_desc[1] += 1
-      curr_bucket_desc[2] = f if f > curr_bucket_desc[2]
-    else
-      buckets << curr_bucket_desc if curr_bucket_desc
-      curr_bucket_desc = [bucket, 1, f]
-      curr_bucket = bucket
+def label(bucket)
+  case bucket
+  when 1
+    return "< 25.0"
+  when 2
+    return "< 35.0"
+  when 3
+    return "< 50.0"
+  when 4
+    return "< 75.0"
+  when 5
+    return "< 95.0"
+  when 6
+    return "≥ 95.0"
+  end
+end
+
+gbuckets = Hash.new(0)
+nsamples = 0
+drops = Hash.new()
+while fname = ARGV.shift
+  de = [0, 0]
+  # Open and read the file
+  ftext = File.read(fname)
+  ftext.each_line { |line|
+    if line =~ /c=([0-9.]+),/
+      str_val = $1
+      f = Float(str_val)
+      bucket = get_bucket(f)
+      next if bucket == 0
+      nsamples += 1
+      gbuckets[bucket] += 1
     end
-  elsif line =~ /Agent starting/
-    buckets << [0] if buckets.count > 0
-  end
+    if line =~ / de=([0-9]+),/
+      de[0] += 1.0
+      de[1] += Float(Integer($1))
+    end
+  }
+  drops.store(fname, de)
+end
+
+# Calculate percentage, then print scaled to 80%
+puts "\nTotal counts\n================"
+gbuckets.keys.sort.each {|k|
+  pct = Float(gbuckets[k]) / Float(nsamples)
+  puts "#{label(k)}: #{'*' * (pct * 80)}"
 }
 
-buckets.each { |bdesc|
-  if bdesc[0] == 0
-    puts "--------------"
-  else
-    puts "#{'%-4d' % bdesc[1]} [#{'%6.2f' % bdesc[2]}]: #{'*' * bdesc[0]}"
-  end
+# Print drops
+puts "\nAvg drops per sample:\n================"
+drops.each { |k, v|
+  puts "#{k}: #{v[1] / v[0]}"
 }
-
-File.write("cpu.csv", values.join(','))
 
