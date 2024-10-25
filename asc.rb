c2a0f842478c0cf3efab97ee8380e6af2f10183e
Nathan Baker <nathan.baker@sysdig.com>
2024-10-25 13:42:18 -0400

    Improved ASCII script

--- asc.rb
+++ asc.rb
@@ -1,6 +1,33 @@
 #!/usr/bin/ruby
 
+p = :char
 while (c = ARGV.shift)
-  puts Integer(c).chr
+  if c == '-c'
+    print = :char
+    c = ARGV.shift
+  elsif c == '-a'
+    print = :val
+    c = ARGV.shift
+  elsif c == '-x'
+    print = :hex
+    c = ARGV.shift
+  elsif c == '-d'
+    print = :dec
+    c = ARGV.shift
+  elsif c =~ /^[0-9]+$/
+    print = :char
+  else
+    print = :val
+  end
+
+  if print == :char
+    puts Integer(c).chr
+  elsif print == :val
+    puts "#{c.ord} (#{c.ord.to_s(16)})"
+  elsif print == :dec
+    puts c.ord
+  elsif print == :hex
+    puts c.ord.to_s(16)
+  end
 end
 
